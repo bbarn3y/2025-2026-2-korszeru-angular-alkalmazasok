d48aac160935dd0871f066c2d86e57ce0294d084
barnabas.balazs <barnabas.balazs@enliven.systems>
2026-02-17 17:17:55 +0100

    2. gyakorlat

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {NzCardModule} from 'ng-zorro-antd/card';
+import {NzFormModule} from 'ng-zorro-antd/form';
+import {NzButtonModule} from 'ng-zorro-antd/button';
+import {NzInputModule} from 'ng-zorro-antd/input';
+import {NzIconModule} from 'ng-zorro-antd/icon';
+import {FormGroup, NonNullableFormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
+import {delay, of} from 'rxjs';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    // Angular
+    ReactiveFormsModule,
+    // Zorro imports
+    NzButtonModule,
+    NzCardModule,
+    NzFormModule,
+    NzIconModule,
+    NzInputModule,
+  ],
   templateUrl: './login.html',
   styleUrl: './login.less',
+  standalone: true,
 })
 export class Login {
+
+  private readonly formBuilder = inject(NonNullableFormBuilder);
+
+  loginForm: FormGroup = this.formBuilder.group({
+    mail: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]]
+  }, { updateOn: 'change' });
+
+  submitLogin() {
+    of({ token: 'My little token' })
+      .pipe(delay(1000))
+      .subscribe((response: { token: string }) => {
+
+      })
+  }
 
 }
