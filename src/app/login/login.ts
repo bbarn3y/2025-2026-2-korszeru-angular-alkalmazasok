e3972b5d6633aa76aaaa16fdf9df02119476368f
barnabas.balazs <barnabas.balazs@enliven.systems>
2026-02-17 17:30:08 +0100

    2. gyakorlat

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -27,16 +27,20 @@ export class Login {
 
   private readonly formBuilder = inject(NonNullableFormBuilder);
 
+  isLoading: boolean = false;
+
   loginForm: FormGroup = this.formBuilder.group({
     mail: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required]]
   }, { updateOn: 'change' });
 
   submitLogin() {
+    this.isLoading = true;
+
     of({ token: 'My little token' })
       .pipe(delay(1000))
       .subscribe((response: { token: string }) => {
-
+        this.isLoading = false;
       })
   }
 
